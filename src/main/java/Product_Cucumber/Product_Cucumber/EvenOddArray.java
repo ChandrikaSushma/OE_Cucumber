f0ce816dd88d6e8af583322cc8b85adb7f4c63b7
ChandrikaSushma <chandrika.kodali2017>
2021-09-30 17:25:42 +0530

    new2

--- src/main/java/Product_Cucumber/Product_Cucumber/EvenOddArray.java
+++ src/main/java/Product_Cucumber/Product_Cucumber/EvenOddArray.java
@@ -3,13 +3,17 @@ package Product_Cucumber.Product_Cucumber;
 public class EvenOddArray {
 
 	public static void main(String[] args) {
-		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 		// forloop
 		for (int i = 0; i < a.length; i++) {
 			if (a[i] % 2 == 0)
 				System.out.println(a[i] + " " + "even");
 		}
 		// enhanced for loop
+		for (int j : a) {
+			if (j % 2 != 0)
+				System.out.println(j + " " + "odd");
+		}
 		for (int j : a) {
 			if (j % 2 != 0)
 				System.out.println(j + " " + "odd");
